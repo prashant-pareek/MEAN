c5a6f252e67f7b56d8c11cc2dd6a5978aa40f79c
Prashant Pareek <prashantpareek1988@gmail.com>
2018-01-06 19:07:12 +0530

    set url routes, set status code and response data

--- README.md
+++ README.md
@@ -6,8 +6,17 @@ __npm start__
 - Starts node application
 
 ## Code Description
-1. Adding express to app
-2. Defining port to listen app
+1. Defining URL routes
+2. Returning status code
+3. Responding with data
+4. Sending HTML response
 
 ## Notes
-Express is a web application framework  
+res.status(200)  
+- Sets response status code
+res.send()
+- respond browser with text data
+res.json()
+- respond with json data
+res.sendFile();
+- respond file
--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 // load express module
 var express = require('express');
 var app = express();
+var path = require('path');
+
+// sending html file from public folder as response
+app.get('/', function(req, res) {
+  console.log('Get the Homepage');
+
+  // here we put public and index.html in separate arguments
+  // reason being different operating system uses different
+  // directory separator
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// set route for get request at path /
+// passing request and response object
+// in callback function
+app.get('/', function(req, res) {
+  console.log('Get the Homepage');
+
+  // send text message to browser
+  //res.send('Express yourself');
+
+  // setting response status code
+  // by chainin status function 
+  // to response object
+  res
+    .status(404)
+    .send('Express yourself');
+});
+
+// seding json response, useful for api response
+app.get('/json', function(req, res) {
+  console.log('Get the json');
+
+  res
+    .status(200)
+    .json({'jsonData': true});
+});
+
+// sending file
+app.get('/file', function(req, res) {
+  console.log('Get the file');
+
+  res
+    .status(200)
+    // join to path variable current directory path
+    // using __dirname and file name
+    .sendFile(path.join(__dirname, 'app.js'));
+});
 
 // set app variable port to 3000
 app.set('port', 3000);
